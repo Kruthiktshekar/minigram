92992060fa066cad9e74b52f1540b76b5921b17b
Kruthiktshekar <kruthik.developer@gmail.com>
2024-10-24 20:16:55 +0530

    updated

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-<<<<<<< HEAD
     baseURL: 'http://localhost:3030/api'
   });
-=======
-    baseURL: 'https://minigram-api.onrender.com/api'
-  });
 
->>>>>>> 698d6060f34c69abfe0bfa04a571c5ead215e490
