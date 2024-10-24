6aa1edc648b3fc9a9c5896ec0658a5bdb2eb2124
Kruthiktshekar <kruthik.developer@gmail.com>
2024-10-24 20:14:59 +0530

    updated

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-<<<<<<< HEAD:frontend/src/axios.js
-    baseURL: 'mingram-api.vercel.app'
-
+    baseURL: 'http://localhost:3030/api'
   });
-=======
-    baseURL: 'https://mingram-api.vercel.app/api'
-  });
->>>>>>> c6073db13b973e62ec5ce76ccb21dc219e54c22c:src/axios.js
