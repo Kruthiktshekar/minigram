e730f6440df5c09f53d9a11cf3d783fd6a2830d4
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-10-24 18:02:26 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-<<<<<<< HEAD:frontend/src/axios.js
-    baseURL: 'mingram-api.vercel.app'
+    baseURL: 'mingram-api.vercel.app/api'
+  });
 
-  });
-=======
-    baseURL: 'https://mingram-api.vercel.app/api'
-  });
->>>>>>> c6073db13b973e62ec5ce76ccb21dc219e54c22c:src/axios.js
