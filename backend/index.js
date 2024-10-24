0c87a298e41c4c25f981f77c37ed1045efb9b83d
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-10-24 18:22:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ dotenv.config()
 const app = express()
 const port = 3030
 app.use(cors({
-  origin: 'https://minigram-kruthiktshekars-projects.vercel.app', 
+  origin: 'https://minigram-phi.vercel.app', 
   methods: ['GET,POST,PUT,DELETE']
   credentials: true 
 }))
