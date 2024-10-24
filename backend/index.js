c6073db13b973e62ec5ce76ccb21dc219e54c22c
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-10-24 17:24:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,11 @@ dotenv.config()
 
 const app = express()
 const port = 3030
-app.use(cors())
+app.use(cors({
+  origin: 'https://mini-blog-app-pi.vercel.app', 
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true 
+}))
 app.use(express.json())
 app.use(route)
 app.use('/upload', express.static(path.join(__dirname, 'upload')));
