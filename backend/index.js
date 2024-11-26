4f8a222d9e9a5ea335024fde8457241304924beb
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-11-26 12:55:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 import configScoket from './config/socket.js'
 
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-dotenv.config()
 
 const app = express()
 const port = 3030
