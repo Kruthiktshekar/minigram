982cd1ceeab1cc3e6c13ecb849d902409aa446b7
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-11-26 09:51:34 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-    baseURL: 'https://minigram-api.onrender.com/api'
+    baseURL: 'https://minigram-api.vercel.app/api'
   });
 
