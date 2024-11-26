c66e92ed69061e79bc51744ac99cb80e75b10e62
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-11-26 13:07:19 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 export const ConfigDb = async () => {
   try {
-    const connection = await mongoose.connect(
-      process.env.MONGODB_URL
+    const connection = await mongoose.connect(process.env.MONGODB_URL,{
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     serverSelectionTimeoutMS: 30000, 
+     }
     );
     if (connection) {
       console.log("Db is connected successfully");
